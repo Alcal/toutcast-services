05dec987e796df48dce27f61e6b9b5e35312cd86
alejandrocastrolopez <acalo91@gmail.com>
2015-08-07 21:59:02 -0700

    Changed nearby reference

--- common/models/tout.js
+++ common/models/tout.js
@@ -31,7 +31,7 @@ module.exports = function(Tout) {
 				{arg:"cap",type:"Number"}
 				],
 			returns:{arg:"touts"},
-			http:{path:"/nearby", verb:"get", status:200, errorStatus:400}
+			http:{verb:"get", status:200, errorStatus:400}
 		}
 	);
 };
