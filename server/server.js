9ad2694c6243a3ab823be175f97d83ab66e9862e
Alejandro Castro Lopez <acalo91@gmail.com>
2016-05-05 20:51:41 -0700

    Added Facebook Auth. Added Redemption Model

--- server/server.js
+++ server/server.js
@@ -3,6 +3,10 @@ var boot = require('loopback-boot');
 
 var app = module.exports = loopback();
 
+//Part of Facebook login configuration
+var PassportConfigurator = require('loopback-component-passport').PassportConfigurator;
+var passportConfigurator = new PassportConfigurator(app);
+
 app.start = function() {
   // start the web server
   return app.listen(function() {
@@ -20,3 +24,31 @@ boot(app, __dirname, function(err) {
   if (require.main === module)
     app.start();
 });
+
+// Enable http session
+app.use(loopback.session({ secret: 'fucking password' }));
+ 
+// Load the provider configurations
+var config = {};
+try {
+ config = require('./providers.json');
+} catch(err) {
+ console.error('Please configure your passport strategy in `providers.json`.');
+ console.error('Copy `providers.json.template` to `providers.json` and replace the clientID/clientSecret values with your own.');
+ process.exit(1);
+}
+// Initialize passport
+passportConfigurator.init();
+
+// Set up related models
+passportConfigurator.setupModels({
+ userModel: app.models.ToutUser,
+ userIdentityModel: app.models.userIdentity,
+ userCredentialModel: app.models.userCredential
+});
+// Configure passport strategies for third party auth providers
+for(var s in config) {
+ var c = config[s];
+ c.session = c.session !== false;
+ passportConfigurator.configureProvider(s, c);
+}
