d4a08560c1c8e9f70ba87e1a014914f44275c18d
AquaBoy <acalo91@gmail.com>
2016-10-14 00:03:06 -0700

    Added test packages and stub test

---
+++ test/stub.js
@@ -0,0 +1,14 @@
+/**
+ * Created by aquaboy on 10/13/16.
+ */
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('stub', function()
+{
+    it('Does nothing', function()
+    {
+        expect('This').to.equal('This');
+    });
+});
